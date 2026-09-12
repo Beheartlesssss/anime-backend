8f60a2b11eb1199817defb1c5508cdf767e6cd64
Beheartlesssss <khushalmahour666@gmail.com>
2026-09-12 15:02:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ from flask import Flask, redirect
 
 app = Flask(__name__)
 
-# Tumhara Bot Token aur Channel ID yahan set hai
+# Tumhara Bot Token
 BOT_TOKEN = "7573887034:AAHLbY9p3S...bhi_jo_tumhara_token_hai..."
-CHANNEL_ID = "@your_channel"
 
 
-@app.route("/stream/<file_id>")
+@app.route("/stream/<path:file_id>")
 def stream_video(file_id):
   try:
     # Telegram API se file ka live path nikalna
@@ -18,11 +17,10 @@ def stream_video(file_id):
 
     if "result" in response and "file_path" in response["result"]:
       file_path = response["result"]["file_path"]
-      # Direct streaming URL generate karke redirect karna
       video_url = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{file_path}"
       return redirect(video_url)
     else:
-      return "Invalid File ID or Bot Token", 400
+      return f"Telegram Error: {response}", 400
   except Exception as e:
     return str(e), 500
 
